a8beb925ef95f4c797ef868b5bd8b9f589ee562c
Michael Akopov <makopov@gmail.com>
2016-03-16 16:02:28 -0400

    PHP Script to compute the matrix norm is done, based on the same design as its C companion. Next is to write a better version of this.

--- compute.php
+++ compute.php
@@ -6,15 +6,21 @@ $matrix = [];
 
 $csvfp = fopen($file, "r");
 
+//print memory_get_usage(true)."\n";
+$count = 1;
 if($csvfp !== false) {
 	while(! feof($csvfp)) {
-		$row = fgetcsv($csvfp, 0, ' '); 
+		$row = fgets($csvfp); 
 		if($row !== false) {
-			$matrix[] = $row;
+			$matrix = array_merge($matrix, explode(' ', $row));
+			//if($count % 10 == 0) { print $count . "\n"; }
+			$count++;
 		}
 		unset($row);
 	}
 }
+
+//print memory_get_usage(true)."\n";
 
 $n = count($matrix);
 $norm = 0;
@@ -23,6 +29,11 @@ $start_time = microtime(true);
 
 $number_of_entries = $n * $n;
 
+foreach($matrix as $value) {
+	$norm += $value * $value;
+}
+
+$norm = sqrt($norm);
 $end_time = microtime(true);
 $total_time = $end_time - $start_time;
 
